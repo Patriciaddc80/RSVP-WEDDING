c6d0409b506b9794ed8df2d17a66aea865d3c61b
Patricia Díaz <98458936+Patriciaddc80@users.noreply.github.com>
2025-05-07 22:13:24 +0200

    first commit project up

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    css: {
-        postcss: './postcss.config.js'
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-    server: {
-        host: true,
-        port: 5173,
-        strictPort: true,
-        open: true
-    }
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+    base: './',
 })
